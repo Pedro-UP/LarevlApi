46bf4b1a0d3d52efd4dba581cbfb1113437ee680
Pedro-UP <pedrocazorla03@gmail.com>
2024-11-26 16:04:37 -0600

    Add Lista de Compras Terminada

--- app/Livewire/ShoppingList.php
+++ app/Livewire/ShoppingList.php
@@ -12,9 +12,9 @@ class ShoppingList extends Component
         return view('livewire.shopping-list');
     }
     public $compras; // Propiedad para almacenar la lista de compras
+    public $total; // Propiedad para almacenar la lista de compras
     public $completada;
-    public $mostrarModal = false; // Nuevo estado para controlar la visibilidad del modal
-
+    public $modoEdicion = false; // Define la variable $modoEdicion
 
     public function mount()
     {
@@ -53,6 +53,7 @@ class ShoppingList extends Component
             });
         }
     }
+    public $comprasSeleccionadas = [];
     public function toggleCompletada($compraId)
     {
         $compra = Compra::find($compraId);
@@ -60,11 +61,81 @@ class ShoppingList extends Component
             $compra->update([
                 'completada' => !$compra->completada,
             ]);
+
+            // Agrega o elimina la compra del array según su estado
+            if ($compra->completada) {
+                $this->comprasSeleccionadas[] = $compraId;
+            } else {
+                $this->comprasSeleccionadas = array_diff($this->comprasSeleccionadas, [$compraId]);
+            }
+
+            // Recalcula el total considerando precio y cantidad de las compras seleccionadas
+            $this->total = $this->calcularTotal();
         }
-        $this->compras = Compra::all();
     }
-    public function openModal($component, $compraId)
+
+    public function calcularTotal()
     {
-        $this->mostrarModal = true;
+        $total = 0;
+        foreach ($this->comprasSeleccionadas as $compraId) {
+            $compra = Compra::find($compraId);
+            if ($compra) {
+                $total += $compra->precio * $compra->cantidad;
+            }
+        }
+        return $total;
+    }
+    public function eliminarComprasSeleccionadas()
+    {
+        // Verifica si hay compras seleccionadas
+        if (count($this->comprasSeleccionadas) >= 0) {
+            // Elimina las compras seleccionadas de la base de datos
+            Compra::whereIn('id', $this->comprasSeleccionadas)->delete();
+            // Filtra la lista de compras actual para eliminar las seleccionadas
+            $this->compras = $this->compras->reject(function ($compra) {
+                return in_array($compra->id, $this->comprasSeleccionadas);
+            });
+            // Limpia el array de compras seleccionadas
+            $this->comprasSeleccionadas = [];
+            // Recalcula el total si es necesario
+            $this->total = $this->calcularTotal();
+        }
+    }
+
+    public function editarCompra($compraId)
+    {
+        // Obtén la compra existente por su ID
+        $compra = Compra::find($compraId);
+        $this->modoEdicion = true;
+        // Actualiza las propiedades del componente con los detalles de la compra
+        $this->compra = [
+            'id' => $compra->id,
+            'nombre' => $compra->nombre,
+            'cantidad' => $compra->cantidad,
+            'precio' => $compra->precio,
+        ];
+    }
+    public function guardarEdicion()
+    {
+        // Actualiza la compra existente con los valores del formulario
+        $compraExistente = Compra::find($this->compra['id']);
+        if ($compraExistente) {
+            $compraExistente->update([
+                'nombre' => $this->compra['nombre'],
+                'cantidad' => $this->compra['cantidad'],
+                'precio' => $this->compra['precio'],
+            ]);
+        }
+
+        // Actualiza la lista de compras
+        $this->compras = Compra::all();
+
+        // Limpia la propiedad $compra y vuelve al estado normal
+        $this->compra = [];
+        $this->modoEdicion = false;
+    }
+    public function cancelarEdicion()
+    {
+        $this->modoEdicion = false;
     }
 }
--- resources/views/livewire/shopping-list.blade.php
+++ resources/views/livewire/shopping-list.blade.php
@@ -1,27 +1,47 @@
-<div>
-    <h2>Lista de compras</h2>
+<div class="bg-stone-500 py-8">
+    <div class="max-w-lg mx-auto p-4 px-4">
+        <h2 class="text-2xl font-bold text-white mb-4">Lista de Compras</h2>
 
-    <ul>
-        @foreach ($compras as $compra)
-            <li>
-                <input type="checkbox" wire:model="compra.{{ $compra->id }}.completada"
-                    wire:click="toggleCompletada({{ $compra->id }})">
-                {{ $compra->nombre }} ({{ $compra->cantidad }}) - ${{ $compra->precio }}
-                <button wire:click="eliminarCompra({{ $compra->id }})">Eliminar</button>
-                <button wire:click="openModal('EditCompraModal', {{ $compra->id }})">Editar</button>
-            </li>
-        @endforeach
-    </ul>
-    <!-- Mostrar el modal solo si $mostrarModal es true -->
-    @if ($mostrarModal)
-        @livewire('edit-compra-modal')
-    @endif
-    <h3>Agregar nueva compra</h3>
+        <div class="bg-white rounded-lg shadow-lg p-6">
+            <ul class="space-y-4">
+                @foreach ($compras as $compra)
+                    <li>
+                        <input type="checkbox" wire:model="compra.{{ $compra->id }}.completada"
+                            wire:click="toggleCompletada({{ $compra->id }})">
+                        {{ $compra->nombre }} ({{ $compra->cantidad }}) - ${{ $compra->precio }}
+                        <button wire:click="eliminarCompra({{ $compra->id }})">Eliminar</button>
+                        <button wire:click="editarCompra({{ $compra->id }})">Editar</button>
+                    </li>
+                @endforeach
 
-    <form wire:submit.prevent="agregarCompra">
-        <input wire:model="nuevaCompra.nombre" name="nombre" placeholder="Nombre" required>
-        <input wire:model="nuevaCompra.cantidad" name="cantidad" placeholder="Cantidad" required>
-        <input wire:model="nuevaCompra.precio" name="precio" step="0.01" placeholder="Precio" required>
-        <button type="submit">Agregar</button>
-    </form>
+                <h3>Total: ${{ number_format($total, 2) }}</h3>
+
+            </ul>
+            <!-- Botón para limpiar compras seleccionadas -->
+            <button wire:click="eliminarComprasSeleccionadas" @if (count($comprasSeleccionadas) == 0) disabled @endif>Limpiar
+                seleccionados</button>
+
+            @if (!empty($compra) && isset($compra['id']) && $modoEdicion)
+                <!-- Mostrar el formulario de edición aquí -->
+                <h3>Editar compra</h3>
+                <form wire:submit.prevent="guardarEdicion">
+                    <input wire:model="compra.nombre" name="nombre" placeholder="Nombre" required>
+                    <input wire:model="compra.cantidad" name="cantidad" placeholder="Cantidad" required>
+                    <input wire:model="compra.precio" name="precio" step="0.01" placeholder="Precio" required>
+                    <button type="submit">Guardar</button>
+                    <button wire:click="cancelarEdicion">Cancelar</button>
+                </form>
+            @endif
+
+
+            <h3>Agregar nueva compra</h3>
+
+            <form wire:submit.prevent="agregarCompra">
+                <input wire:model="nuevaCompra.nombre" name="nombre" placeholder="Nombre" required>
+                <input wire:model="nuevaCompra.cantidad" name="cantidad" placeholder="Cantidad" required>
+                <input wire:model="nuevaCompra.precio" name="precio" step="0.01" placeholder="Precio" required>
+                <button type="submit">Agregar</button>
+            </form>
+        </div>
+    </div>
 </div>
